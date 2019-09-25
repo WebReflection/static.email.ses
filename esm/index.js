4b1857e2ebdc11adb9965346cb2c007f745fc7c4
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-09-25 17:55:27 +0100

    trying reCAPTCHA

--- esm/index.js
+++ esm/index.js
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk';
+import {post} from 'faroff';
 import marked from 'marked';
 import safe from 'escape-string-regexp';
 
@@ -6,9 +7,10 @@ const {isArray} = Array;
 const {keys} = Object;
 
 const statuses = {
-  '200': 'OK',
-  '400': 'Bad Request',
-  '500': 'Internal Server Error'
+  200: 'OK',
+  400: 'Bad Request',
+  403: 'Forbidden',
+  500: 'Internal Server Error'
 };
 
 const end = (response, code) => {
@@ -31,8 +33,10 @@ export const create = ({
   apiVersion,       // the SES API version, default "2010-12-01"
   charset,          // the optional charset to use, default "UTF-8"
   log,              // logs success/error operations, default false
+  recaptcha,        // secret key for communication between your site and reCAPTCHA
   senderReply       // the "from" email address, default no-reply
 }) => {
+  const RECAPTCHA = !!recaptcha;
   const LOG = !!log;
   const Charset = charset || 'UTF-8';
   const Destination = {ToAddresses: isArray(to) ? to : to.split(/\s*,\s*/)};
@@ -47,53 +51,83 @@ export const create = ({
   return (request, response) => {
     try {
       if (request.method !== 'POST' || !referer.test(request.headers.referer))
-        throw null;
+        throw 400;
 
       const {body} = request;
       if (typeof body !== 'object')
-        throw null;
+        throw 400;
 
       const details = keys(body);
-      const withText = details.includes('text');
-      const withHTML = details.includes('html');
-      const withMD = details.includes('md');
+      if (RECAPTCHA && !details.includes('recaptcha') || !body.recaptcha)
+        throw 403;
 
-      if (!(withText || withHTML || withMD))
-        throw null;
-
-      const Body = {};
-      const params = {
-        Source, Destination,
-        Message: {
-          Body, Subject: {
-            Charset,
-            Data: (body.subject || '').trim() || 'No Subject'
+      const send = () => {
+        const withText = details.includes('text');
+        const withHTML = details.includes('html');
+        const withMD = details.includes('md');
+  
+        if (!(withText || withHTML || withMD))
+          throw 400;
+  
+        const Body = {};
+        const params = {
+          Source, Destination,
+          Message: {
+            Body, Subject: {
+              Charset,
+              Data: (body.subject || '').trim() || 'No Subject'
+            }
           }
+        };
+  
+        if (withText)
+          Body.Text = {Charset, Data: body.text};
+  
+        if (withMD) {
+          const {md} = body;
+          Body.Html = {Charset, Data: marked(md)};
+          if (!withText)
+            Body.Text = {Charset, Data: md};
         }
+        else if (withHTML)
+          Body.Html = {Charset, Data: body.html};
+  
+        if (details.includes('from'))
+          params.ReplyToAddresses = [body.from];
+  
+        new AWS.SES(SESConfig).sendEmail(params).promise().then(
+          data => { if (LOG) console.log(data); end(response, 200); },
+          err => { if (LOG) console.error(err, err.stack); end(response, 500); }
+        );
       };
 
-      if (withText)
-        Body.Text = {Charset, Data: body.text};
-
-      if (withMD) {
-        const {md} = body;
-        Body.Html = {Charset, Data: marked(md)};
-        if (!withText)
-          Body.Text = {Charset, Data: md};
+      if (RECAPTCHA) {
+        post('https://www.google.com/recaptcha/api/siteverify', {
+          query: {
+            secret: recaptcha,
+            response: body.recaptcha,
+            remoteip: request.headers['x-forwarded-for'] || 
+                      request.connection.remoteAddress
+          }
+        }).then(
+          result => {
+            if (result.success)
+              send();
+            else {
+              if (LOG) console.error(result);
+              end(response, 403);
+            }
+          },
+          error => {
+            end(response, 500);
+          }
+        );
       }
-      else if (withHTML)
-        Body.Html = {Charset, Data: body.html};
-
-      if (details.includes('from'))
-        params.ReplyToAddresses = [body.from];
-
-      new AWS.SES(SESConfig).sendEmail(params).promise().then(
-        data => { if (LOG) console.log(data); end(response, 200); },
-        err => { if (LOG) console.error(err, err.stack); end(response, 500); }
-      );
+      else
+        send();
     }
     catch (o_O) {
-      end(response, 400);
+      end(response, o_O);
     }
   };
 };
